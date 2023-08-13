603c16b9c1dae044b303fe8aef5837331df8588c
Andrei <asd@mail.com>
2023-08-13 18:51:30 +0300

    filters added

--- src/Home.js
+++ src/Home.js
@@ -2,25 +2,21 @@ import { useState } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-
-    const[name, setName] = useState('Mario');
-    const[age, setAge] = useState(25);
     const [blogs, setBlogs] = useState([
     { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
     { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
     { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
     ]);
 
-    const handleClick = (name) => {
-        setName('Luigi');
-        setAge(30);
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id !== id);
+        setBlogs(newBlogs);
     }
     return ( 
         <div className="home">
             <h2>Homepage</h2>
-            <BlogList blogs={blogs} title="All blogs" />
-            <p>{ name } is { age } years old.</p>
-            <button onClick={handleClick}>Click me</button>
+            <BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete}/>
+            <BlogList blogs={blogs.filter((blogs) => blogs.author === 'mario')} title="My blogs" />
         </div>
      );
 }
